8eea9dfa8233f87550a0c83d01ad5c3d02a8e642
chancee12 <128835183+chancee12@users.noreply.github.com>
2023-07-19 16:19:11 -0400

    Update 12_🌲_Land_Cover_Mapping.py

--- pages/12_🌲_Land_Cover_Mapping.py
+++ pages/12_🌲_Land_Cover_Mapping.py
@@ -20,9 +20,6 @@ st.sidebar.info(
     
     Axim Homepage:
     [Axim Geospatial](https://www.aximgeo.com/)
-    
-    Additional Thank You, Qiusheng Wu:
-    [LinkedIn](https://www.linkedin.com/in/qiushengwu/)    
     """
 )
 
