43340b4e5dbc9347d1abd6139df3efa6af9d682e
chancee12 <128835183+chancee12@users.noreply.github.com>
2023-07-25 15:53:47 -0400

    s

--- apps/proposal_chatbot.py
+++ apps/proposal_chatbot.py
@@ -48,6 +48,7 @@ def main_page():
     user_input = st.text_area("Paste the text you'd like Chancee's AI Bot to revise:", height=200)
     submit_button = st.button("Submit")
 
+<<<<<<< HEAD
     default_prompt = "Revise Axim's proposal sections for heightened clarity, structure, alignment with the proposal's specific requirements, and professional appeal, keeping in line with government contract proposal standards. If any sections contain parenthetical revision notes, such as (Make more relatable to clients' unique problems), integrate these revisions distinctly as per the comment. Apply principles of clarity, conciseness, order, and persuasion in your revision without explicitly mentioning them. Your tone should remain professional and concise, avoiding filler words, and the final text length should not deviate significantly from the original. Proceed with the following text for revision:"
 
     if prompt_input == '':
@@ -70,5 +71,37 @@ def main_page():
         st.markdown("### **Revision Explanation:**")
         st.text_area("", value=revision_explanation, height=200)
 
+=======
+def main_page():
+    prompt_input = st.text_input("Enter the prompt for the AI:")
+    user_input = st.text_area("Paste the text you'd like Chancee's AI Bot to revise:", height=200)
+    submit_button = st.button("Submit")
+
+    # Set a default prompt
+    default_prompt = "Revise Axim's proposal sections for heightened clarity, structure, alignment with the proposal's specific requirements, and professional appeal, keeping in line with government contract proposal standards. If any sections contain parenthetical revision notes, such as (Make more relatable to clients' unique problems), integrate these revisions distinctly as per the comment. Apply principles of clarity, conciseness, order, and persuasion in your revision without explicitly mentioning them. Your tone should remain professional and concise, avoiding filler words, and the final text length should not deviate significantly from the original. Proceed with the following text for revision:"
+
+    # If the user hasn't provided a prompt, use the default
+    if prompt_input == '':
+        prompt_input = default_prompt
+
+    if submit_button and user_input:
+        with st.spinner("Generating revised text..."):
+            revised_text, revision_length, revision_explanation = revise_text(prompt_input, user_input)
+            time.sleep(1)  # Simulating some processing time, remove this line in the actual implementation
+
+        st.markdown("### **Original Text:**")
+        st.text_area("", value=user_input, height=200, max_chars=None, key=None)
+
+        st.markdown("### **Revised Text:**")
+        st.text_area("", value=revised_text, height=200, max_chars=None, key=None)  # Changed to text_area
+
+        st.markdown("### **Revision Length:**")
+        st.code(revision_length, language='')
+
+        st.markdown("### **Revision Explanation:**")
+        st.text_area("", value=revision_explanation, height=200, max_chars=None, key=None)
+
+
+>>>>>>> parent of de5d6b7 (added chatbot app)
 if __name__ == "__main__":
     main_page()
