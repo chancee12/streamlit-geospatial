19f20ff1bd756be3bf6f58248e15f83a54cd1e4d
chancee12 <128835183+chancee12@users.noreply.github.com>
2023-07-26 14:36:17 -0400

    editacronymbot

--- pages/1_🤖_Acronym_Builder_Chatbot.py
+++ pages/1_🤖_Acronym_Builder_Chatbot.py
@@ -1,49 +1,15 @@
 import streamlit as st
-import openai
-import time
-import re
+import app
 
-def check_password():
-    """Returns `True` if the user had the correct password."""
+if app.check_password():
+    st.sidebar.title("Contact")
+    st.sidebar.info(
+        """
+        [Chancee]:
+        [LinkedIn]
+        """
+    )
 
-    def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        if st.session_state["password"] == st.secrets["password"]:
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # don't store password
-        else:
-            st.session_state["password_correct"] = False
-
-    if "password_correct" not in st.session_state:
-        # First run, show input for password.
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
-        return False
-    elif not st.session_state["password_correct"]:
-        # Password not correct, show input + error.
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
-        st.error("😕 Password incorrect")
-        return False
-    else:
-        # Password correct.
-        return True
-
-def extract_acronyms(text):
-    """Extract acronyms and their definitions from the given text."""
-    pattern = re.compile(r'\(([A-Z]{2,})\)')
-    acronyms = pattern.findall(text)
-    definitions = {}
-
-    for acronym in acronyms:
-        definition_pattern = re.compile(r'([a-zA-Z0-9\s,]+)\s*\(' + acronym + '\)')
-        definitions[acronym] = definition_pattern.findall(text)
-
-    return definitions
-
-def app():
     st.title("Acronym Extraction Bot Beta V.1.0.0")
     st.markdown(
     """
@@ -61,7 +27,7 @@ def app():
 
     if submit_button and user_input:
         with st.spinner("Loading..."):
-            acronyms_and_definitions = extract_acronyms(user_input)
+            acronyms_and_definitions = app.extract_acronyms(user_input)
             time.sleep(1)
 
         if acronyms_and_definitions:
@@ -70,14 +36,3 @@ def app():
                 st.write(f"{acronym}: {', '.join(definition)}")
         else:
             st.markdown("### **No acronyms found.**")
-
-if check_password():
-    st.sidebar.title("Contact")
-    st.sidebar.info(
-        """
-        [Chancee]:
-        [LinkedIn]
-        """
-    )
-
-    app()
